5a4a0fbe8fcfafc9b6a7068ed2c5c1eed0f39516
tserikawa <tkt.srkw@gmail.com>
2026-09-19 16:41:50 +0900

    フォーマットを修正

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 use anyhow::Result;
 use axum::{extract::State, http::StatusCode, routing::get, Router};
-use tokio::net::TcpListener;
 use sqlx::{postgres::PgConnectOptions, PgPool};
+use tokio::net::TcpListener;
 
-async fn hello_world() -> &'static str{
+async fn hello_world() -> &'static str {
     "Hello, World!"
 }
 
-pub async fn health_check() -> StatusCode
-{
+pub async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
@@ -19,14 +18,14 @@ async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
     let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
     match connection_result {
         Ok(_) => StatusCode::OK,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // データベースの接続設定
-    let database_cfg = DatabaseConfig{
+    let database_cfg = DatabaseConfig {
         host: "localhost".into(),
         port: 5432,
         username: "app".into(),
@@ -36,7 +35,8 @@ async fn main() -> Result<()> {
     // コネクションプール
     let conn_pool = connect_database_with(database_cfg);
 
-    let app = Router::new().route("/hello", get(hello_world))
+    let app = Router::new()
+        .route("/hello", get(hello_world))
         .route("/health_check", get(health_check))
         .route("/health_check_db", get(health_check_db))
         .with_state(conn_pool);
@@ -47,8 +47,7 @@ async fn main() -> Result<()> {
 }
 
 #[tokio::test]
-async fn test_check_works()
-{
+async fn test_check_works() {
     let status_code = health_check().await;
     assert_eq!(status_code, StatusCode::OK);
 }
@@ -59,12 +58,12 @@ struct DatabaseConfig {
     pub port: u16,
     pub username: String,
     pub password: String,
-    pub database: String
+    pub database: String,
 }
 
 /// アプリケーション用のデータベース設定構造体から、Postgres接続用の構造体へ変換する
-impl From<DatabaseConfig> for PgConnectOptions{
-    fn from(cfg: DatabaseConfig) -> Self{
+impl From<DatabaseConfig> for PgConnectOptions {
+    fn from(cfg: DatabaseConfig) -> Self {
         Self::new()
             .host(&cfg.host)
             .port(cfg.port)
@@ -75,12 +74,12 @@ impl From<DatabaseConfig> for PgConnectOptions{
 }
 
 /// Postgres専用のコネクションプールを作成する。
-fn connect_database_with(cfg: DatabaseConfig) -> PgPool{
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
     PgPool::connect_lazy_with(cfg.into())
 }
 
 #[sqlx::test]
-async fn health_check_db_works(pool: sqlx::PgPool){
+async fn health_check_db_works(pool: sqlx::PgPool) {
     // 引数のPgPoolは環境変数のDATABASE_URLから作成される。
     //
     let status_code = health_check_db(State(pool)).await;
